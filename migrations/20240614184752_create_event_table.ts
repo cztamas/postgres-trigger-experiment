4bc8009bc1807729aa9f5f7bd41fb561d9fb0aba
cztamas <tamas.czank@gmail.com>
2024-06-14 18:54:11 +0000

    create tables

---
+++ migrations/20240614184752_create_event_table.ts
@@ -0,0 +1,14 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('event', table => {
+    table.uuid('id').primary();
+    table.string('type');
+    table.string('user_id');
+    table.dateTime('timestamp');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('event');
+}
---
+++ migrations/20240614185113_create_message_table.ts
@@ -0,0 +1,14 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('message_outbox', table => {
+    table.uuid('id').primary();
+    table.string('user_id');
+    table.string('content');
+    table.dateTime('created_at');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('message_outbox');
+}
